bfd27282521288dd0af210e0170322cc26f6e58d
Reim-developer <contact.kaxtr@gmail.com>
2025-02-18 19:30:24 +0700

    Added new module find_element and new command find to find element of website and print this info, if exist

---
+++ src/core/find_element.rs
@@ -0,0 +1,96 @@
+use std::time::{Duration, Instant};
+
+use crate::styles::colors::LogLevel;
+use anyhow::{anyhow, Context, Result};
+use indicatif::{ProgressBar, ProgressStyle};
+use prettytable::{format, Cell, Row, Table};
+use reqwest::{header::USER_AGENT, Client};
+use scraper::{Html, Selector};
+
+/// Find HTML element of website and print this info, if exist.
+/// # Example
+/// ```rust
+/// let my_website = "https://example.com";
+/// let my_css_query = "div#example"; // div tag with example id
+///
+/// element_count(my_website, my_css_query);
+///
+/// ```
+/// # Parameters:
+/// - `website` Website you want to check.
+/// - `element` Your CSS query. Use * to query all elements.
+///
+pub async fn find_element(website: &str, element: &str) -> Result<()> {
+    let client = Client::new();
+    let error_color = LogLevel::Error.fmt();
+    let success_color = LogLevel::Success.fmt();
+    let process_bar = ProgressBar::new_spinner();
+    let mut table = Table::new();
+
+    let start_time = Instant::now();
+    table.set_format(*format::consts::FORMAT_BOX_CHARS);
+    table.add_row(Row::new(vec![
+        Cell::new("Element Type"),
+        Cell::new("Element ID"),
+        Cell::new("Element Class"),
+    ]));
+
+    match ProgressStyle::default_spinner()
+        .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"])
+        .template("{spinner} {msg}")
+    {
+        Ok(style) => process_bar.set_style(style),
+        Err(error) => {
+            eprintln!(
+                "{} Cannot set template of process bar with error {}",
+                error_color, error
+            );
+        }
+    }
+
+    process_bar.set_message("Processing...");
+    process_bar.enable_steady_tick(Duration::from_millis(100));
+
+    let response = client.get(website)
+    .header(USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+    .send().await.with_context(|| format!("{} Cannot get response", error_color))?;
+
+    let html_content = response
+        .text()
+        .await
+        .with_context(|| format!("{} Cannot get HTML source", error_color))?;
+
+    let website_source = Html::parse_document(&html_content);
+
+    let selecto_query = Selector::parse(element)
+        .map_err(|error| anyhow!("{} Cannot parse element with error {}", error_color, error))?;
+
+    let mut elements = website_source.select(&selecto_query).peekable();
+
+    if elements.peek().is_none() {
+        eprintln!("{} No matching element found.", error_color);
+        return Err(anyhow!("NO_MATCHING_ELEMENT_FOUND"));
+    }
+
+    for element in elements {
+        let element_type = element.value().name();
+        let element_class = element.value().attr("class").unwrap_or("No class");
+        let element_id = element.value().attr("id").unwrap_or("No id");
+
+        table.add_row(Row::new(vec![
+            Cell::new(element_type),
+            Cell::new(element_id),
+            Cell::new(element_class),
+        ]));
+    }
+
+    process_bar.suspend(|| {
+        table.printstd();
+    });
+    let end_time = start_time.elapsed();
+    
+    process_bar.finish_and_clear();
+    println!("{} Finished in {:.2?}", success_color, end_time);
+
+    Ok(())
+}
