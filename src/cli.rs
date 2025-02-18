ce01d1ab8984da9571a1efea4f8c721a820337a0
Reim-developer <contact.kaxtr@gmail.com>
2025-02-18 11:20:07 +0700

    Added Enum CommandLineInterface

---
+++ src/cli.rs
@@ -0,0 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct CommandLineInterface {
+    #[command(subcommand)]
+    pub commands: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    #[command(name = "match")]
+    Match {
+        #[arg(short = 'w', long = "web")]
+        website: String,
+        #[arg(short = 'e', long = "element")]
+        element: String,
+    },
+}
