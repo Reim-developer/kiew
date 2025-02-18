bc73a6ba84222dc9a976093adcf89b08fe061ca1
Reim-developer <contact.kaxtr@gmail.com>
2025-02-18 12:15:28 +0700

    Added color to error handles

--- src/cmds/handles.rs
+++ src/cmds/handles.rs
@@ -3,19 +3,18 @@ use clap::Parser;
 use crate::{
     cli::{CommandLineInterface, Commands},
     core::element::element_count,
+    styles::colors::LogLevel,
 };
-
 
 /// Handles all CLI commmands
 pub async fn handles_commands() {
     let args = CommandLineInterface::parse();
+    let error_color = LogLevel::Error.fmt();
 
     match args.commands {
-        Commands::Match { website, element } => {
-            match element_count(&website, &element).await {
-                Ok(()) => {},
-                Err(error) => eprintln!("Get element of website error: {}", error),
-            }
-        }
+        Commands::Match { website, element } => match element_count(&website, &element).await {
+            Ok(()) => {}
+            Err(error) => eprintln!("{} Get element of website error: {}", error_color, error),
+        },
     }
 }
