c04ca3b031ec772b2aa12fb64edcf51c0477f818
Reim-developer <contact.kaxtr@gmail.com>
2025-02-18 12:15:50 +0700

    Added color and fixed error handling in  this module

--- src/core/element.rs
+++ src/core/element.rs
@@ -1,6 +1,8 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use reqwest::Client;
 use scraper::Html;
+
+use crate::styles::colors::LogLevel;
 
 /// Counter the number of elements matching a CSS query on a given website.
 ///
@@ -18,23 +20,30 @@ use scraper::Html;
 ///
 pub async fn element_count(website: &str, element: &str) -> Result<()> {
     let client = Client::new();
+    let error_color = LogLevel::Error.fmt();
+    let success_color = LogLevel::Success.fmt();
+    let warn_color = LogLevel::Warning.fmt();
 
     let response = client.get(website).header(reqwest::header::USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-    .send().await?;
+    .send().await.with_context(|| format!("{} Cannot get response", error_color))?;
 
-    let html_content = response.text().await?;
+    let html_content = response
+        .text()
+        .await
+        .with_context(|| format!("{} Cannot get HTML source", error_color))?;
 
     let document = Html::parse_document(&html_content);
 
-    let selector_query = scraper::Selector::parse(element)
-        .map_err(|error| anyhow::anyhow!("Cannot parse element with error {}", error))?;
+    let selector_query = scraper::Selector::parse(element).map_err(|error| {
+        anyhow::anyhow!("{} Cannot parse element with error {}", error_color, error)
+    })?;
 
     let element_count = document.select(&selector_query).count();
 
     match element_count {
-        0 => println!("Could not find any element."),
-        1 => println!("Find one element: {}.", element_count),
-        _ => println!("Found many elements: {}.", element_count),
+        0 => println!("{} Could not find any element.", warn_color),
+        1 => println!("{} Found one {} element: {}.", success_color, element, element_count),
+        _ => println!("{} Found many {} elements: {}.", success_color, element, element_count),
     }
 
     Ok(())
