9b018b1311e05fe9bacd858c40be345399250e0d
Reim-developer <contact.kaxtr@gmail.com>
2025-02-19 14:35:52 +0700

    Refactor CLI module and added comment

--- src/cli.rs
+++ src/cli.rs
@@ -1,24 +1,34 @@
 use clap::{Parser, Subcommand};
 
+/// Define all commands of CLI
 #[derive(Parser)]
 pub struct CommandLineInterface {
+    /// Define sub-commands.
     #[command(subcommand)]
     pub commands: Commands,
 }
 
+/// s
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Match command
     #[command(name = "match")]
     Match {
+        /// This website target
         #[arg(short = 'w', long = "web")]
         website: String,
         #[arg(short = 'e', long = "element")]
+        /// Element you want match
         element: String,
     },
+
+    /// Find command
     #[command(name = "find")]
     Find {
+        /// This website target
         #[arg(short = 'w', long = "web")]
         website: String,
+        /// This element you want match
         #[arg(short = 'e', long = "element")]
         element: String,
     },
