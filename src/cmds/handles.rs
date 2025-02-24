7b3e87e4b97d841fb000d44aecc10f0e54103e1e
Reim-developer <contact.kaxtr@gmail.com>
2025-02-24 19:07:42 +0700

    Removed all println! Migration to macro stdout

--- src/cmds/handles.rs
+++ src/cmds/handles.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use crate::{
     cli::{CommandLineInterface, Commands, OptionsScraping::Href},
     colors::LogLevel,
-    core::{crawl_href::href_scraper, element::element_count, find_element::find_element},
+    core::{crawl_href::href_scraper, element::element_count, find_element::find_element}, fatal,
 };
 
 /// Handles all CLI commmands
@@ -14,22 +14,22 @@ pub async fn handles_commands() {
     match args.commands {
         Commands::Match { website, element } => match element_count(&website, &element).await {
             Ok(()) => {}
-            Err(error) => eprintln!("{error_color} Fatal: {error}"),
+            Err(error) => fatal!("{error_color} Fatal: {error}"),
         },
-
+        
         Commands::Find {
             website,
             element,
             debug_mode,
         } => match find_element(&website, &element, &debug_mode).await {
             Ok(()) => {}
-            Err(error) => eprintln!("{error_color} Fatal: {error}"),
+            Err(error) => fatal!("{error_color} Fatal: {error}"),
         },
 
         Commands::Crawl { options } => match options {
             Href { website_url, debug } => {
                 if let Err(error) = href_scraper(&website_url, &debug).await {
-                    eprintln!("{error_color} Fatal: {error}");
+                    fatal!("{error_color} Fatal: {error}");
                 }
             }
         },
