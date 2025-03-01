22097a282fe16ca1058d9b336176bcaf84bb835e
Reim-developer <contact.kaxtr@gmail.com>
2025-03-01 16:54:43 +0700

    Added new command for CLI: "post"

--- src/cli.rs
+++ src/cli.rs
@@ -89,17 +89,37 @@ pub enum Commands {
         #[arg(short = 'd', long = "debug", default_value = "false")]
         debug_option: bool,
     },
+
+    /// POST request command
+    #[command(name = "post")]
+    Post {
+        /// Website URL
+        #[arg(short = 'w', long = "web")]
+        website_url: String,
+
+        /// Custom headers for request
+        #[arg(short = 'H', value_parser = parse_header)]
+        headers: Vec<(HeaderName, HeaderValue)>,
+
+        /// Custom payload for POST request
+        #[arg(short = 'P', default_value = "")]
+        payload: String,
+
+        /// Debug option
+        #[arg(short = 'd', long = "debug", default_value = "false")]
+        debug_option: bool,
+    },
 }
 
 /// Parser header
-fn parse_header(s: &str) -> Result<(HeaderName, HeaderValue), anyhow::Error> {
-    let parts: Vec<&str> = s.splitn(2, ": ").collect();
+fn parse_header(headers: &str) -> Result<(HeaderName, HeaderValue), anyhow::Error> {
+    let parts: Vec<&str> = headers.splitn(2, ": ").collect();
     let key_str = parts
         .first()
-        .ok_or_else(|| anyhow!("Missing header KEY in {}", s))?;
+        .ok_or_else(|| anyhow!("Missing header KEY in {}", headers))?;
     let value_str = parts
         .get(1)
-        .ok_or_else(|| anyhow!("Missing header VALUE in {}", s))?;
+        .ok_or_else(|| anyhow!("Missing header VALUE in {}", headers))?;
 
     let key = HeaderName::from_str(key_str)?;
     let value = HeaderValue::from_str(value_str)?;
