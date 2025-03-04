6c132e3815baaf7989af3973dc8f90245b1482aa
Reim-developer <contact.kaxtr@gmail.com>
2025-03-04 17:08:13 +0700

    Removed deprecated module

--- src/lib.rs
+++ src/lib.rs
@@ -430,16 +430,6 @@
 
 /// Core, is logic of Command Line application.
 pub mod core {
-    /// To match a element of website
-    pub mod element;
-
-    /// To find element of website and show
-    /// all infomation, if this exist
-    pub mod find_element;
-
-    /// Scraper all href URL in website
-    pub mod crawl_href;
-
     /// Logging system
     pub mod log;
 
