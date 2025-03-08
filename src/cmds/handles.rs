6f186053138b158ae4e9ac5dee121f761df30f33
Reim-developer <contact.kaxtr@gmail.com>
2025-03-08 15:19:09 +0700

    Fixed new parameters

--- src/cmds/handles.rs
+++ src/cmds/handles.rs
@@ -2,9 +2,14 @@ use anyhow::anyhow;
 use clap::Parser;
 
 use crate::{
-    cli::{CommandLineInterface, Commands}, core::http::{
-        delete::delete_request, get::{match_options_get, match_setting_get}, post::post_request, put::put_request,
-    }, ultis::setting::generate_setting
+    cli::{CommandLineInterface, Commands},
+    core::http::{
+        delete::delete_request,
+        get::{match_options_get, match_setting_get},
+        post::post_request,
+        put::put_request,
+    },
+    ultis::setting::generate_setting,
 };
 
 /// Handles all CLI commmands
@@ -31,27 +36,22 @@ pub async fn handles_commands() -> Result<(), anyhow::Error> {
                 if !web.is_empty() && !setting.is_empty() {
                     return Err(anyhow!("Cannot use '-S' or '--web' argument one time"));
                 }
-            } 
+            }
 
             if let Some(ref web_url) = website_url {
                 if web_url.is_empty() {
                     return Err(anyhow!("Website URL is empty"));
                 } else if !web_url.is_ascii() {
-                    return Err(anyhow!("Website URL is not ASCII format"))
+                    return Err(anyhow!("Website URL is not ASCII format"));
                 }
                 match_options_get(web_url, &headers, debug_option, details).await?;
             }
 
             if let Some(settings) = read_setting {
-                if settings.is_empty() {
-                    return Err(anyhow!("Website URL is empty"));
-                } else if !settings.is_ascii() {
-                    return Err(anyhow!("Website URL is not ASCII format"))
+                if !settings.is_empty() {
+                    match_setting_get(&settings).await?;
                 }
-
-                match_setting_get(&settings)?;
             }
-
         }
         Commands::Post {
             website_url,
